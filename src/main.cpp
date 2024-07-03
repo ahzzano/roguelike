41d417cfeb833df02d1081c6bf2e9852d6610932
W. Santos <w.santos.gg@gmail.com>
2024-07-03 11:04:44 +0800

    hello glfw + window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
-#define SDL_MAIN_HANDLED
+// #define SDL_MAIN_HANDLED
 
-#include <SDL2/SDL.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+#define ASSETS_PATH "./assets";
+
 int main(int argc, char *argv[]) {
-  std::cout << "Hello World!";
+  std::cout << ASSETS_PATH;
+
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
