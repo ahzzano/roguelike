98b1542b540ad17bcae856a78f898c324ab0b0e5
W. Santos <w.santos.gg@gmail.com>
2024-07-03 11:39:44 +0800

    hello opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 // #define SDL_MAIN_HANDLED
 
+#include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 #define ASSETS_PATH "./assets";
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                         int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
 
 int main(int argc, char *argv[]) {
   std::cout << ASSETS_PATH;
@@ -17,7 +26,22 @@ int main(int argc, char *argv[]) {
 
   glfwMakeContextCurrent(window);
 
+  int version = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+  if (version == 0) {
+    std::cout << "Failed to initialize OpenGL Context\n";
+    return -1;
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+
+  int width, height;
   while (!glfwWindowShouldClose(window)) {
+
+    glfwGetFramebufferSize(window, &width, &height);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
